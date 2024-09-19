97553424cf55d635249743319fb880d4504a24f2
Onur Yalçın <onuryalcin@Onur-MacBook-Pro.local>
2024-09-19 16:16:40 -0400

    cors enable

--- api/server.js
+++ api/server.js
@@ -11,6 +11,14 @@ const server = jsonServer.create()
 // const db = JSON.parse(data);
 // const router = jsonServer.router(db)
 
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 // Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
